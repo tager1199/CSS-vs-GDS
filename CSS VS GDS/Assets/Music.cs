ab95b82fa5b5d11987468b06c897f011986c893c
tager1199 <17643144@students.lincoln.ac.uk>
2019-03-10 10:06:06 +0000

    Music
    Added music to game

---
+++ CSS VS GDS/Assets/Music.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+
+
+    AudioSource audioData;
+
+
+        // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        audioData = GetComponent<AudioSource>();
+        audioData.Play(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
